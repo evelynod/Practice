1a8084ddc2cbd41d06014a1c5e453684cd2483e1
evelynod <odonnell.ev@gmail.com>
2025-07-29 19:53:20 -0700

    Add variable to control mana regeneration amount

--- methods_01.py
+++ methods_01.py
@@ -1,5 +1,6 @@
 class Wizard:
     mana = 50
+    
 
     def cast_spell(self, cost):
         self.mana = self.mana - cost
@@ -7,15 +8,16 @@ class Wizard:
 
 
 
-    def regenerate_mana(self):
-        self.mana += 1      #increases mana by 1
-        print(f"Mana Regeneration! Fizzy's mana is now {self.mana}!")
+    def regenerate_mana(self, mana_regen_amount):
+        self.mana += mana_regen_amount 
+        print(f"Mana Regeneration of {mana_regen_amount}! \nFizzy's mana is now {self.mana}!")
 
 Fizzy = Wizard()  
 cost = 5
 Fizzy.cast_spell(cost)  
+mana_regen_amount = 2
 
-Fizzy.regenerate_mana()
+Fizzy.regenerate_mana(mana_regen_amount)
 
 
 
@@ -23,3 +25,7 @@ Fizzy.regenerate_mana()
 # A challenge from Boots on July 29, 2025
 # Write a class called "Wizard" with an attribute called mana (start it at 50)
 # Then, if you create a wizard and call cast_spell, the mana will decrease as intended!  
+
+# A challenge from Boots: Add a regenerate mana method that increases Fizzy's mana by a certain
+# amount.
+# Now allow that regenerate mana amount to be changed.
