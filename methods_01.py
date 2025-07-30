f4d8cdc8a14210de1df211946fb5571ceca29edd
evelynod <odonnell.ev@gmail.com>
2025-07-29 19:26:05 -0700

    Add regenerate mana method to Wizard class

--- methods_01.py
+++ methods_01.py
@@ -5,9 +5,21 @@ class Wizard:
         self.mana = self.mana - cost
         print(f"Fizzy has {self.mana} mana left!")
 
+
+
+    def regenerate_mana(self):
+        self.mana += 1      #increases mana by 1
+        print(f"Mana Regeneration! Fizzy's mana is now {self.mana}!")
+
 Fizzy = Wizard()  
 cost = 5
-Fizzy.cast_spell(cost)   
+Fizzy.cast_spell(cost)  
+
+Fizzy.regenerate_mana()
 
 
 
+# Fizzy Wizard 
+# A challenge from Boots on July 29, 2025
+# Write a class called "Wizard" with an attribute called mana (start it at 50)
+# Then, if you create a wizard and call cast_spell, the mana will decrease as intended!  
